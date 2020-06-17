750aad498c34144c548916770ce19074d06c5c2d
Paulus <paulus@gewirr.cc>
2020-06-17 14:33:00 +0300

    ls-1738 Extracted common part

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,9 @@
-
-set fish_greeting ""
-set EDITOR emacsclient -a emacs
-set PATH ~/.cargo/bin ~/.go/bin $PATH
+set -x fish_greeting ""
+set -x EDITOR emacsclient -a emacs
+set -x PATH $PATH ~/.cargo/bin ~/.go/bin $PATH ~/.local/bin
+set -x IDF_PATH ~/projects/esp-idf/
+set -x BAT_THEME OneHalfLight
+set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh"
 set -x GOPATH ~/.go
 
 abbr ec emacsclient -a emacs
